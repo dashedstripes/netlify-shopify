c3f134771cbbb6abcf6f36a7f335efbf54adbc8c
Adam Gray <adamgray@Adams-MacBook-Pro.local>
2023-09-11 15:34:38 -0500

    basic

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,13 +1,25 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import { shopifyFetch } from '@/utils/shopifyFetch'
 import type { NextApiRequest, NextApiResponse } from 'next'
 
 type Data = {
   name: string
 }
 
-export default function handler(
+export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
+
+  const products = await shopifyFetch({
+    query: `
+      query {
+        products {
+          id
+        }
+      }
+    `,
+    variables: ''
+  })
   res.status(200).json({ name: 'John Doe' })
 }
